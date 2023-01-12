f707301e1e7b347b6af1592fbf120d4790d37a82
Franklin Kipchumba <sitieneifranklin@gmail.com>
2023-01-12 18:07:06 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -6,17 +6,52 @@ function App() {
   const[count, setCount] = useState(0);
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  class DarkMode extends React.Component {
+    state = {
+       setClass: JSON.parse(localStorage.getItem("Mode"))
+     };
+   
+     handleChange = () => {
+   
+       if (JSON.parse(localStorage.getItem("Mode")) === true)
+         this.setState({ setClass: false }, () => {
+           localStorage.setItem("Mode", JSON.stringify(false));
+         });
+       else {
+         this.setState({ setClass: true }, () => {
+           localStorage.setItem("Mode", JSON.stringify(true));
+         });
+       }
+    };
+   
+    render() {
+      const { setClass } = this.state;
+  
 
-  return (
-    <div className={appClass}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={() => setCount(count + 1)}>Dark Mode</button>
-      </header>
-      <ShoppingList items={itemData} />
-    </div>
-  );
+      return (
+        <div className={appClass}>
+          <div className="App dark">
+            {/* for dark mode */}
+          </div>
+
+          <div className="App light">
+            {/* for light mode */}
+          </div>
+          <header>
+          <h2>Shopster</h2>
+          <button onClick={() => setCount(count + 1)}>(mode)</button>
+          </header>
+          <ShoppingList items={itemData} />
+          <div className={setClass ? "dark-mode" : "light-mode"}>
+          <nav>Dark Mode</nav>
+          <main>
+          <h1>Light Mode</h1>
+          <button onClick={this.handleChange}>Switch</button>
+          </main>
+          </div>
+        </div>
+      );
+    }
+  }
 }
-
 export default App;
